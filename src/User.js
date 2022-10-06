c2d695b349155a18252b37721c4be0d4d472ab07
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-10-06 18:06:05 +0530

    Initial commit

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+        <img src='https://th.bing.com/th/id/OIP.jnVG8iyv2wNyZKFrZoaTZwHaE7?pid=ImgDet&rs=1' alt='user'/>
+    </div>
+  )
+}
+
+export default User
